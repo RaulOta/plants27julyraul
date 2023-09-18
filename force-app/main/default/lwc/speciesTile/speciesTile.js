25a056347031af5b11caea5f3c89f91adad5e62d
Raul Otanez <raul10.r77@gmail.com>
2023-09-18 11:50:15 -0500

    Add lightning icon with conditional if

--- force-app/main/default/lwc/speciesTile/speciesTile.js
+++ force-app/main/default/lwc/speciesTile/speciesTile.js
@@ -2,4 +2,16 @@ import { LightningElement, api } from 'lwc';
 
 export default class SpeciesTile extends LightningElement {
   @api specie;
+
+  // specie.Location__c = "Indoors";
+  // specie.Location__c = "Outdoors";
+  // specie.Location__c = "Indoors,Outdoors";
+
+  get isOutdoors(){
+    return this.specie.Location__c.includes("Outdoors");
+  }
+
+  get isIndoors(){
+    return this.specie.Location__c.includes("Indoors");
+  }
 }
